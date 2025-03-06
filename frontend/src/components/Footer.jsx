1ccfda94f816c2e82f8840fdc3a848ae727f7a23
shivansh kamboj <kambojshivansh2004@gmail.com>
2025-03-06 16:41:55 +0530

    h

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -24,17 +24,14 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>8708606784</li>
+            <li>shivansh1221651@jmit.ac.in</li>
           </ul>
         </div>
 
       </div>
 
-      <div>
-        <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
-      </div>
+
 
     </div>
   )
