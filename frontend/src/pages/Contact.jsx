5f25de4da158012d9dffce6accb57829118e092a
shivansh kamboj <kambojshivansh2004@gmail.com>
2025-03-06 16:39:58 +0530

    changes

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,8 +13,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Jmit Radaur</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: shivansh1221651@jmit.ac.in</p>
+          <p className=' text-gray-500'>IT department <br /> Jmit Radaur</p>
+          <p className=' text-gray-500'>Tel: 8708606784<br /> Email: shivansh1221651@jmit.ac.in</p>
           <p className=' font-semibold text-lg text-gray-600'>CAREERS AT MEDICARE</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
