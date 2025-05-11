3bcaa113565357b99296162b6586b178ac6d517d
shivansh kamboj <kambojshivansh2004@gmail.com>
2025-05-11 18:32:04 +0530

    fixed image glitch

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    image: { type: String, default: '' },
+    image: { type: String, default: 'https://res.cloudinary.com/dreirplqv/image/upload/v1746968479/default-avatar-icon-of-social-media-user-vector_ce6sgj.jpg' },
     phone: { type: String, default: '000000000' },
     address: { type: Object, default: { line1: '', line2: '' } },
     gender: { type: String,enum:["Male","Female","Not Selected"], default: 'Not Selected' },
-    dob: { type: String, default: 'Not Selected' },
+    dob: { type: String },
     password: { type: String, required: true },
 })
 
